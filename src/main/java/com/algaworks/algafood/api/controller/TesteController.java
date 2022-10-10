8566d7242a8414ae053523c3cb42c454d9efeb92
Alice <alicesilvademeneses@gmail.com>
2022-10-10 11:45:35 -0300

    Mudar consulta de restaurante por nome e por cozinha id para usar JṔQL invés do query method para melhorar a legibilidade do metódo

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -43,7 +43,7 @@ public class TesteController {
 	
 	@GetMapping("/restaurantes/por-nome-e-cozinha-id")
 	public List<Restaurante> buscarPorNomeECozinhaId(@RequestParam String nome, @RequestParam Long cozinhaId) {
-		return restauranteRepository.findByNomeContainingAndCozinhaId(nome, cozinhaId);
+		return restauranteRepository.consultarPorNomeECozinhaId(nome, cozinhaId);
 	}
 	
 	@GetMapping("/restaurantes/primeiro-por-nome")
--- src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/RestauranteRepository.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.algaworks.algafood.domain.model.Restaurante;
@@ -14,7 +15,10 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long>{
 
 	List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaFreteInicial, BigDecimal taxaFreteFinal);
 
-	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long id);
+	@Query("select r from Restaurante r where nome like %:nome% and cozinha.id = :cozinhaId")
+	List<Restaurante> consultarPorNomeECozinhaId(String nome, Long cozinhaId);
+	
+	//	List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 	
 	Optional<Restaurante> getFirstByNomeContaining(String nome);
 	
