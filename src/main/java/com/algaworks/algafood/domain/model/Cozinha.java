2d6903381c2262892acd7194c3c35a56b1afd0e2
Alice <alicesilvademeneses@gmail.com>
2022-10-15 18:05:41 -0300

    Mapeando relacionamento bidirecional com restaurante. Os restaurantes da cozinha são ignorados na serialização, pois sem isso ocorreria um loop sem fim devido o relacionamento bidirecional

--- src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,10 +1,15 @@
 package com.algaworks.algafood.domain.model;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -22,4 +27,8 @@ public class Cozinha {
 	@Column(nullable = false)
 	private String nome;
 
+	@JsonIgnore
+	@OneToMany
+	private List<Restaurante> restaurantes;
+	
 }
