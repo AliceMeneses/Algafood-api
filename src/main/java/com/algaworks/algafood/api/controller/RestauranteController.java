59ba6d2007a23beeab8f4b9848ffc8f0f42c71a6
Alice <alicesilvademeneses@gmail.com>
2022-10-29 09:29:58 -0300

    Arrumar código de status HTTP para 400 quando uma cozinha de um restaurante não existe e mesmo assim o cliente tenta salvar ou atualizar o restaurante com essa cozinha

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -16,6 +16,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.exception.NegocioException;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
@@ -43,7 +45,11 @@ public class RestauranteController {
 
 	@PostMapping
 	public Restaurante salvar(@RequestBody Restaurante restaurante) {
-		return cadastroRestaurante.salvar(restaurante);
+		try {
+			return cadastroRestaurante.salvar(restaurante);
+		} catch (EntidadeNaoEncontradaException e) {
+			throw new NegocioException(e.getMessage());
+		}
 	}
 
 	@PutMapping("/{id}")
@@ -52,7 +58,11 @@ public class RestauranteController {
 	    BeanUtils.copyProperties(restaurante, restauranteAtual, 
 	            "id", "formasPagamento", "endereco", "dataCadastro", "produtos");
 	    
-	    return cadastroRestaurante.salvar(restauranteAtual);
+	    try {
+		    return cadastroRestaurante.salvar(restauranteAtual);
+	    } catch (EntidadeNaoEncontradaException e) {
+	    	throw new NegocioException(e.getMessage());
+		}
 	}
 
 	@PatchMapping("/{id}")
