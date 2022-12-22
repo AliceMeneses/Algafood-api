49a1602d2c3bed65c7787f71ce2f055f18b38e45
AliceMeneses <alicesilvademeneses@gmail.com>
2022-12-22 15:49:50 -0300

    Tratar exceção de parâmetro de URL inválido

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/ProblemType.java
@@ -8,7 +8,8 @@ public enum ProblemType {
 	ENTIDADE_NAO_ENCONTRADA("/entidade-nao-encontrada", "Entidade não encontrada"),
 	ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso"),
 	ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
-	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem Incompreensível");
+	MENSAGEM_INCOMPREENSIVEL("/mensagem-incompreensivel", "Mensagem Incompreensível"),
+	PARAMETRO_INVALIDO("/parametro-invalido", "Parâmetro inválido");
 	
 	String uri;
 	String title;
