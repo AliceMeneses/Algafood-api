b99331b6c4c5ce1cc5a2c93ccd4c9e77deb54e52
AliceMeneses <alicesilvademeneses@gmail.com>
2023-02-11 18:34:09 -0300

    Melhorando solução que impede HibernateException de ser lançado

--- src/main/java/com/algaworks/algafood/api/assembler/CidadeInputDisassembler.java
+++ src/main/java/com/algaworks/algafood/api/assembler/CidadeInputDisassembler.java
@@ -1,18 +1,23 @@
 package com.algaworks.algafood.api.assembler;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import com.algaworks.algafood.api.model.input.CidadeInput;
 import com.algaworks.algafood.domain.model.Cidade;
-import com.algaworks.algafood.domain.model.Estado;
 
 @Component
 public class CidadeInputDisassembler {
 
     @Autowired
     private ModelMapper modelMapper;
+    
+    @PersistenceContext
+    private EntityManager manager;
     
     public Cidade toDomainObject(CidadeInput cidadeInput) {
         return modelMapper.map(cidadeInput, Cidade.class);
@@ -21,8 +26,7 @@ public class CidadeInputDisassembler {
     public void copyToDomainObject(CidadeInput cidadeInput, Cidade cidade) {
         // Para evitar org.hibernate.HibernateException: identifier of an instance of 
         // com.algaworks.algafood.domain.model.Estado was altered from 1 to 2
-        cidade.setEstado(new Estado());
-        
+        manager.detach(cidade.getEstado());
         modelMapper.map(cidadeInput, cidade);
     }
     
