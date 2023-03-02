a233510fc9491d586ec51b81710b1994b8e4e371
AliceMeneses <alicesilvademeneses@gmail.com>
2023-03-02 10:19:21 -0300

    Adicionar CRUD do usuario

--- src/main/java/com/algaworks/algafood/api/controller/UsuarioController.java
+++ src/main/java/com/algaworks/algafood/api/controller/UsuarioController.java
@@ -2,9 +2,10 @@ package com.algaworks.algafood.api.controller;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,64 +18,66 @@ import org.springframework.web.bind.annotation.RestController;
 import com.algaworks.algafood.api.assembler.UsuarioInputDisassembler;
 import com.algaworks.algafood.api.assembler.UsuarioModelAssembler;
 import com.algaworks.algafood.api.model.UsuarioModel;
+import com.algaworks.algafood.api.model.input.SenhaInput;
+import com.algaworks.algafood.api.model.input.UsuarioComSenhaInput;
 import com.algaworks.algafood.api.model.input.UsuarioInput;
-import com.algaworks.algafood.api.model.input.UsuarioSemSenhaInput;
 import com.algaworks.algafood.domain.model.Usuario;
+import com.algaworks.algafood.domain.repository.UsuarioRepository;
 import com.algaworks.algafood.domain.service.CadastroUsuarioService;
 
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
 
-	@Autowired
-	private CadastroUsuarioService cadastroUsuario;
-	
-	@Autowired
-	private UsuarioModelAssembler usuarioModelAssembler;
-	
-	@Autowired
-	private UsuarioInputDisassembler usuarioInputDisassembler;
-	
-	@GetMapping
-	private List<UsuarioModel> listar() {
-		List<Usuario> usuarios = cadastroUsuario.listar();
-		return usuarioModelAssembler.toCollectionModel(usuarios);
-	}
-	
-	@GetMapping("/{id}")
-	public UsuarioModel buscar(@PathVariable Long id) {
-		Usuario usuario = cadastroUsuario.buscar(id);
-		return usuarioModelAssembler.toModel(usuario);
-	}
-	
-	@PostMapping
-	@ResponseStatus(HttpStatus.CREATED)
-	public UsuarioModel adicionar(@RequestBody UsuarioInput usuarioInput) {
-		Usuario usuario = usuarioInputDisassembler.toDomainObject(usuarioInput);
-		usuario = cadastroUsuario.salvar(usuario);
-		return usuarioModelAssembler.toModel(usuario);
-	}
-	
-	@PutMapping("/{id}")
-	public UsuarioModel atualizar(@PathVariable Long id, @RequestBody UsuarioSemSenhaInput usuarioSemSenhaInput) {
-		Usuario usuario = cadastroUsuario.buscar(id);
-		usuarioInputDisassembler.copyToDomainObject(usuarioSemSenhaInput, usuario);
-		usuario = cadastroUsuario.salvar(usuario);
-		return usuarioModelAssembler.toModel(usuario);
-	}
-	
-	@PutMapping("/{id}/senha")
-	public UsuarioModel atualizarSenha(@PathVariable Long id, @RequestBody UsuarioSenhaInput usuarioInput) {
-		Usuario usuario = cadastroUsuario.buscar(id);
-		usuarioInputDisassembler.copyToDomainObject(usuarioInput, usuario);
-		usuario = cadastroUsuario.salvar(usuario);
-		return usuarioModelAssembler.toModel(usuario);
-	}
-	
-	@DeleteMapping("/{id}")
-	@ResponseStatus(HttpStatus.NO_CONTENT)
-	public void remover(@PathVariable Long id) {
-		cadastroUsuario.remover(id);
-	}
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+    
+    @Autowired
+    private CadastroUsuarioService cadastroUsuario;
+    
+    @Autowired
+    private UsuarioModelAssembler usuarioModelAssembler;
+    
+    @Autowired
+    private UsuarioInputDisassembler usuarioInputDisassembler;
+    
+    @GetMapping
+    public List<UsuarioModel> listar() {
+        List<Usuario> todasUsuarios = usuarioRepository.findAll();
+        
+        return usuarioModelAssembler.toCollectionModel(todasUsuarios);
+    }
+    
+    @GetMapping("/{usuarioId}")
+    public UsuarioModel buscar(@PathVariable Long usuarioId) {
+        Usuario usuario = cadastroUsuario.buscarOuFalhar(usuarioId);
+        
+        return usuarioModelAssembler.toModel(usuario);
+    }
+    
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UsuarioModel adicionar(@RequestBody @Valid UsuarioComSenhaInput usuarioInput) {
+        Usuario usuario = usuarioInputDisassembler.toDomainObject(usuarioInput);
+        usuario = cadastroUsuario.salvar(usuario);
+        
+        return usuarioModelAssembler.toModel(usuario);
+    }
+    
+    @PutMapping("/{usuarioId}")
+    public UsuarioModel atualizar(@PathVariable Long usuarioId,
+            @RequestBody @Valid UsuarioInput usuarioInput) {
+        Usuario usuarioAtual = cadastroUsuario.buscarOuFalhar(usuarioId);
+        usuarioInputDisassembler.copyToDomainObject(usuarioInput, usuarioAtual);
+        usuarioAtual = cadastroUsuario.salvar(usuarioAtual);
+        
+        return usuarioModelAssembler.toModel(usuarioAtual);
+    }
+    
+    @PutMapping("/{usuarioId}/senha")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void alterarSenha(@PathVariable Long usuarioId, @RequestBody @Valid SenhaInput senha) {
+        cadastroUsuario.alterarSenha(usuarioId, senha.getSenhaAtual(), senha.getNovaSenha());
+    }
 	
 }
