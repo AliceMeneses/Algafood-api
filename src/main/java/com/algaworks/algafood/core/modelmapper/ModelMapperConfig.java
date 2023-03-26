13e914a9c8536cf45d0eb6bc532ea3b9a2829f6b
AliceMeneses <alicesilvademeneses@gmail.com>
2023-03-26 17:02:24 -0300

    Adicionar emissão de pedido

--- src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/algaworks/algafood/core/modelmapper/ModelMapperConfig.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import com.algaworks.algafood.api.model.EnderecoModel;
+import com.algaworks.algafood.api.model.input.ItemPedidoInput;
 import com.algaworks.algafood.domain.model.Endereco;
+import com.algaworks.algafood.domain.model.ItemPedido;
 
 @Configuration
 public class ModelMapperConfig {
@@ -19,6 +21,9 @@ public class ModelMapperConfig {
 		enderecoToEnderecoModelTypeMap.<String>addMapping(
 				enderecoSrc -> enderecoSrc.getCidade().getEstado().getNome(), 
 				(enderecoDest, nomeEstado) -> enderecoDest.getCidade().setEstado(nomeEstado));
+		
+		modelMapper.createTypeMap(ItemPedidoInput.class, ItemPedido.class)
+	    	.addMappings(mapper -> mapper.skip(ItemPedido::setId)); 
 		
 		return modelMapper;
 	}
